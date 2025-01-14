c73285b76f081df62269c128881414d2403c074a
developersview <pcslg1998@gmail.com>
2025-01-15 04:51:34 +0530

    56 days of sql - problem 10

---
+++ 56DaysOfSQL/sql_problem10_mysql_solution.sql
@@ -0,0 +1,56 @@
+/*
+Write a query that'll identify returning active users. 
+A returning active user is a user that has made a second purchase within 7 days of any other of their purchases. 
+Output a list of user_ids of these returning active users.
+*/
+USE sqlpractice;
+
+DROP TABLE IF EXISTS amazon_transactions;
+CREATE TABLE amazon_transactions(
+	id int, 
+	user_id int, 
+	item varchar(15), 
+	created_at datetime, 
+	revenue int
+);
+
+INSERT INTO amazon_transactions VALUES (1,109,'milk','2020-03-03 00:00:00',123),
+(2,139,'biscuit','2020-03-18 00:00:00', 421), (3,120,'milk','2020-03-18 00:00:00',176), 
+(4,108,'banana','2020-03-18 00:00:00',862), (5,130,'milk','2020-03-28 00:00:00',333), 
+(6,103,'bread','2020-03-29 00:00:00',862), (7,122,'banana','2020-03-07 00:00:00',952), 
+(8,125,'bread','2020-03-13 00:00:00',317), (9,139,'bread','2020-03-30 00:00:00',929), 
+(10,141,'banana','2020-03-17 00:00:00',812), (11,116,'bread','2020-03-31 00:00:00',226), 
+(12,128,'bread','2020-03-04 00:00:00',112), (13,146,'biscuit','2020-03-04 00:00:00',362), 
+(14,119,'banana','2020-03-28 00:00:00',127), (15,142,'bread','2020-03-09 00:00:00',503), 
+(16,122,'bread','2020-03-06 00:00:00',593), (17,128,'biscuit','2020-03-24 00:00:00',160), 
+(18,112,'banana','2020-03-24 00:00:00',262), (19,149,'banana','2020-03-29 00:00:00',382), 
+(20,100,'banana','2020-03-18 00:00:00',599);
+
+SELECT * FROM amazon_transactions;
+
+-- solution 1
+WITH cte_next_purchase AS(
+	SELECT 
+		user_id, 
+		created_at,
+		LAG(created_at) OVER(PARTITION BY user_id ORDER BY created_at DESC) AS next_purchase
+	FROM 
+		amazon_transactions
+)
+SELECT 
+	user_id
+FROM 
+	cte_next_purchase
+WHERE 
+	DATEDIFF(next_purchase, created_at) <= 7;
+
+-- solution 2
+SELECT DISTINCT
+    a.user_id
+FROM
+    amazon_transactions a
+        JOIN
+    amazon_transactions b ON a.user_id = b.user_id
+        AND a.created_at < b.created_at
+WHERE
+    DATEDIFF(b.created_at, a.created_at) <= 7;
