d4cf98f1533785a9c972e1e3fc3f944e2db97cec
developersview <pcslg1998@gmail.com>
2025-02-01 18:51:23 +0530

    56 days of sql - problem 44

---
+++ 56DaysOfSQL/sql_problem44_mssql_mysql_solution.sql
@@ -0,0 +1,18 @@
+/*
+Amazon (Hard Level)
+You are given the table with titles of recipes from a cookbook and their page numbers.
+You are asked to represent how the recipes will be distributed in the book.
+Produce a table consisting of three columns: left_page_number, left_title and right_title. 
+The k-th row (counting from 0), should contain the number and the title of the page with the number 2×k in the first and second columns respectively, 
+and the title of the page with the number 2×k+1 in the third column.
+*/
+USE sqlpractice;
+
+CREATE TABLE cookbook_titles (
+	page_number INT PRIMARY KEY,
+	title VARCHAR(255)
+);
+
+INSERT INTO cookbook_titles (page_number, title) VALUES 
+(1, 'Scrambled eggs'), (2, 'Fondue'), (3, 'Sandwich'), (4, 'Tomato soup'), 
+(6, 'Liver'), (11, 'Fried duck'), (12, 'Boiled duck'), (15, 'Baked chicken');
