2c803994c2852f2be2a43a19d369edb4838ee647
developersview <pcslg1998@gmail.com>
2025-02-02 02:40:44 +0530

    56 days of sql - problem 45

--- 56DaysOfSQL/sql_problem44_mssql_mysql_solution.sql
+++ 56DaysOfSQL/sql_problem44_mssql_mysql_solution.sql
@@ -8,6 +8,7 @@ and the title of the page with the number 2×k+1 in the third column.
 */
 USE sqlpractice;
 
+DROP TABLE IF EXISTS cookbook_titles;
 CREATE TABLE cookbook_titles (
 	page_number INT PRIMARY KEY,
 	title VARCHAR(255)
@@ -16,3 +17,32 @@ CREATE TABLE cookbook_titles (
 INSERT INTO cookbook_titles (page_number, title) VALUES 
 (1, 'Scrambled eggs'), (2, 'Fondue'), (3, 'Sandwich'), (4, 'Tomato soup'), 
 (6, 'Liver'), (11, 'Fried duck'), (12, 'Boiled duck'), (15, 'Baked chicken');
+
+SELECT * FROM cookbook_titles;
+
+-- solution
+WITH LeftPage AS(
+	SELECT
+		page_number,
+		title
+	FROM
+		cookbook_titles
+	WHERE page_number % 2 = 0
+),
+RightPage AS(
+	SELECT
+		page_number,
+		title
+	FROM
+		cookbook_titles
+	WHERE page_number % 2 = 1
+)
+SELECT
+	lp.page_number,
+	lp.title AS left_page_title,
+	rp.title AS right_page_title
+FROM
+	LeftPage lp
+		LEFT JOIN
+	RightPage rp ON lp.page_number + 1 = rp.page_number
+ORDER BY lp.page_number;
